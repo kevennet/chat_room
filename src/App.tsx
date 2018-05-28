91d68e9b28f0f3a2a1e55b0f5d5748c2ed7eb183
Inori <337938188@qq.com>
2018-05-28 16:28:59 +0800

    loadable示例

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,14 @@ import './App.css';
 import { hot } from 'react-hot-loader';
 import logo from './logo.svg';
 
+import * as Loadable from 'react-loadable';
+import Loading from './my-loading-component';
+
+const LoadableComponent = Loadable({
+  loader: () => import('./my-component'),
+  loading: Loading,
+});
+
 class App extends React.Component {
   public render() {
     return (
@@ -15,6 +23,7 @@ class App extends React.Component {
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <LoadableComponent/>
       </div>
     );
   }
---
+++ src/my-loading-component.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { hot } from 'react-hot-loader';
+import * as Loadable from "react-loadable";
+
+class App extends React.Component<Loadable.LoadingComponentProps> {
+  public render() {
+    return (
+      <div>
+        loading
+      </div>
+    );
+  }
+}
+
+export default hot(module)(App);
