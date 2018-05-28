68f4959cfabd659efb57673a48c3d96f972a1862
Inori <337938188@qq.com>
2018-05-28 19:21:07 +0800

    调整排版

--- src/compents/head.tsx
+++ src/compents/head.tsx
@@ -1,13 +1,8 @@
 import Icon from "antd-mobile/lib/icon"
-
 import "antd-mobile/lib/icon/style/css"
-
 import NavBar from "antd-mobile/lib/nav-bar"
-
 import 'antd-mobile/lib/nav-bar/style/css'
-
 import React, { Fragment, PureComponent } from 'react'
-
 import './head/index.css'
 
 
@@ -21,7 +16,6 @@ class PageHeader extends PureComponent {
         >
           聊天室
         </NavBar>
-        <div className="inoritest">test</div>
       </Fragment>
     )
   }
