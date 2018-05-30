694ee47228b4ea848726c8ecd5809f172b337d01
Inori <337938188@qq.com>
2018-05-30 18:52:07 +0800

    添加class定义

--- src/compents/message.tsx
+++ src/compents/message.tsx
@@ -1,12 +1,49 @@
-import React, {PureComponent, ReactChildren} from 'react'
+import React, {ReactChild, PureComponent} from 'react'
 
+
+type InitialProps = {
+  /**
+   * @desc 对话内容
+   */
+  content?: string,
+  /**
+   * @desc 个人头像
+   */
+  logo?: string,
+  /**
+   * @desc 个人简介
+   */
+  summary?: string,
+  /**
+   * @desc 时间戳
+   */
+  time?: Number,
+  /**
+   * @desc 我的还是别人的
+   */
+  type?: 'me' | 'others' | ReactChild,
+}
+const initialProps: InitialProps = {
+  content: '对话内容',
+  logo: '',
+  summary: '',
+  time: +new Date,
+  type: 'me'
+}
+
+/**
+ * @class 单个对话框
+ */
 class MessageItem extends PureComponent {
-  constructor (props: string, children: ReactChildren) {
-    super(props)
+  constructor (props: InitialProps = {}) {
+    const mergedProps: InitialProps = Object.assign(props, initialProps)
+    super(mergedProps)
   }
   public render() {
     return (
-      <span>{this.props.children}</span>
+      <div>
+        <span>{this.props.children}</span>
+      </div>
     )
   }
 }
